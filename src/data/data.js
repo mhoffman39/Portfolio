9898571d64e54497db997d0ec6a8eb8ae1750f1a
Mason Hoffman <mhoffman3939@gmail.com>
2021-08-19 10:23:33 -0700

    Edited resume link and added tech stack

--- src/data/data.js
+++ src/data/data.js
@@ -4,8 +4,8 @@ import { nanoid } from 'nanoid';
 export const aboutData = {
   paragraphOne: 'I recently completed a 21 year career in the US Navy submarine force. It was an exciting and rewarding career where I learned about electronics, leadership, and working with people from diverse backgrounds. During my time in the Navy, I also fell in love with computers and technology and began pursuing a degree in cybersecurity.',
   paragraphTwo: 'When I officially retired from the Navy, I decided I wanted to build software. I felt that software engineering was a great combination of two of my passions: computers and building things. I recently completed the Hack Reactor Software Engineering Immersive, an arduous 12 week program where I learned Javascript and full stack web development. ',
-  paragraphThree: 'When I\'m not building software, I enjoy ripping down hills on my mountain bike, camping in the beautiful Pacific Northwest, home remodeling projects, or reading a good book.',
-  resume: 'https://drive.google.com/file/d/1ZkxLHkXLqkMy3t06x1hl9Sy7g3hKTT-k/view?usp=sharing',
+  paragraphThree: 'My ever-expanding tech stack includes: Javascript, ReactJS, NodeJS, Express, PostgreSQL, MongoDB, AWS, HTML5, and CSS. ,
+  resume: 'https://drive.google.com/file/d/https://docs.google.com/document/d/1ypLg7ibA9_yfet4N5TZ7_l76H2Cz_stuqjX_vLR5KVE/edit?usp=sharing-k/view?usp=sharing',
 };
 
 // PROJECTS DATA
@@ -14,9 +14,9 @@ export const projectsData = [
     id: nanoid(),
     img: 'goFishMovies.png',
     title: 'Go Fish!',
-    info: 'Your favorite app for when you and your friends can\'t decide which movie to watch. This social media application lets users search for their friends and their favorite movies. Then, when you can\'t agree on a movie just hit the "Go Fish" button. The application will return a random movie that you and your friend have both selected as a favorite.',
-    //url: 'https://www.gofishmovies.com',
-    url: '',
+    info: 'Your favorite app for when you and your friends can\'t decide which movie to watch. This social media application lets users search for their friends and their favorite movies. Then, when you can\'t agree on a movie just hit the "Go Fish" button. The application will return a random movie that you and your friend have both selected as a favorite. ',
+    url: 'https://www.gofishmovies.com',
+    // url: '',
     repo: 'https://github.com/Team-Go-Fish/Go-Fish',
   },
   {
